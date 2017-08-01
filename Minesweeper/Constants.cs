a3ffbaa5eee970dfa1794853e688873eb4cc87f4
Murat Kuru <muratkuru@users.noreply.github.com>
2017-08-01 17:44:56 +0300

    Update Constants.cs

--- Minesweeper/Constants.cs
+++ Minesweeper/Constants.cs
@@ -6,10 +6,6 @@ namespace Minesweeper
     {
         public const int BUTTON_SIZE = 32;
 
-        public const string SUCCESSFUL_MESSAGE = "Congratulations!";
-
-        public const string MESSAGE_TITLE = "Game Over";
-
         public static Color[] Colors = { Color.Black, Color.Blue, Color.Orange, Color.Olive, Color.Orchid, Color.DarkGray, Color.Purple, Color.Purple, Color.LightCyan, Color.Yellow };
     }
 }
